6c2fd63d9a2265482f38e43afd687f8710bd5dfd
ManuelAlcaz101 <111898585+ManuelAlcaz101@users.noreply.github.com>
2025-04-23 11:20:53 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,22 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ Private Arraylist<Inteser> my Daily StePs;
+ Private int active; e
+ public StepTracker (int steps)
+My Vailysters = new Array Lisa < Intesers ();
+active = steps;
+3
+Public void addDailySteps (int steps)
+ myDailySteps.add(Steps);
+Public int active Dals lE
+int dals=o,
+for (int i =0; i < My DailySters. Size C); it+) if (i →= active ) days++; 3 return days;
+Public double averageStepscE
+if(my Daily StePS. Size () = =0)
+return o;
+Louble steps= o;
+forCint i=0; i <my Pailysteps, sized); it+)
+steps++;
+return steps/my Dailysters. sizec);
 } 
